37a7b5606bd0dea6de0fd7ad82280d48a06c4c5f
Kerim <shukurovkerim050@gmail.com>
2025-01-16 12:30:28 +0500

    Add files via upload

--- 4.py
+++ 4.py
@@ -1,10 +1,47 @@
+# a)
 telefon_belgi = {
 "Anna" : "63188199",
 "Gurban" : "63115599",
 "Oraz" : "62111444"}
 print(telefon_belgi)
 
+# b)
 print(telefon_belgi["Anna"])
 
+# c)
 sorag = input("Kimin telefon belgisi gerek ? ")
-print([sorag])
+if sorag in telefon_belgi:
+    print(f"{sorag} - yn telefon belgisi: {telefon_belgi[sorag]}")
+else:
+    print(f"{sorag} - yn telefon belgisi tapylmady !")
+
+# d)
+print(len(telefon_belgi))
+
+# e)
+telefon_belgi["Gurban"] = "63625495"
+print(telefon_belgi)
+
+# f)
+telefon_belgi["Muhhamet"] = "63384289"
+print(telefon_belgi)
+
+# g)
+telefon_belgi.pop("Gurban")
+print(telefon_belgi)
+
+# h)
+for i in telefon_belgi.keys():
+    print(i)
+
+# i)
+for i in telefon_belgi.values():
+    print(i)
+
+# j)
+for i,k in telefon_belgi.items():
+    print(i,k)
+
+# k)
+telefon_belgi.clear()
+print(telefon_belgi)
